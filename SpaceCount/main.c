a55c49dd01e2ae958aeb16e678a98942e035147f
Daniel Buckle <danielbuckle31@gmail.com>
2013-04-28 17:47:14 +0100

    Initial Commit

---
+++ SpaceCount/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  SpaceCount
+//
+//  Created by Daniel Buckle on 28/04/2013.
+//  Copyright (c) 2013 Daniel Buckle. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
